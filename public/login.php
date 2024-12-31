6249838babfdbcd7162406dddf9ad0f9beeedc22
EliaBergamin <eliberg031009@gmail.com>
2024-12-31 12:24:18 +0100

    aggiunta funzionalità di redirect

--- public/login.php
+++ public/login.php
@@ -1,50 +1,20 @@
 <?php
-/* function clean_input($data)
-{
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
-session_start();
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (array_key_exists("email", $_POST))
-        $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
-    if (array_key_exists("password", $_POST))
-        $password = clean_input($_POST["password"]);
-
-    include "connect.php";
-
-    $stmt = $connection->prepare("SELECT * FROM users WHERE email = :email");
-    $stmt->bind_param(':username', $email);
-    $stmt->execute();
-    $user = $stmt->get_result()->fetch_assoc();
-
-    if ($user && password_verify($password, $user['password'])) {
-        // password valida
-        $_SESSION['user_id'] = $user['id'];
-        $_SESSION['username'] = $user['username'];
-        $_SESSION['role'] = $user['role'];
-        header("Location: ../index.php");
-        exit;
-    } else {
-        echo "Credenziali incorrette";
-    }
-} */
 
 require_once("phplibs/databaseService.php");
 require_once("phplibs/templatingService.php");
 
-
-
-if (isset($_SESSION['user_id'])) 
-    header("Location: index.php");
+$redirect = $_GET['redirect'] ?? 'index.php';
+if (isset($_SESSION['user_id']))
+    header("Location: $redirect");
 
 $database = new DatabaseService();
 $loginHtmlContent = Templating::getHtmlWithModifiedMenu(__FILE__);
-if (!$loginHtmlContent) {}
-    //TODO
+if (!$loginHtmlContent) {
+}
+//TODO
+$formToModify = Templating::getContentBetweenPlaceholders($loginHtmlContent, 'form');
+Templating::replaceAnchor($formToModify, 'redirect', $redirect);
+Templating::replaceContentBetweenPlaceholders($loginHtmlContent, 'form', $formToModify);
 Templating::showHtmlPageWithoutPlaceholders($loginHtmlContent);
 
 ?>
