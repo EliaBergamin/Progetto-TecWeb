5c6c592995b0b2233ea2cfdd3c2ae374d5ba6ca7
EliaBergamin <eliberg031009@gmail.com>
2024-12-31 11:57:08 +0100

    aggiunte tutte le mostre alla tabella nella sezione admin; migliorati i controlli di sessione

--- public/admin.php
+++ public/admin.php
@@ -4,22 +4,27 @@ require_once("phplibs/templatingService.php");
 
 
 
-if (!isset($_SESSION['user_id']) && $_SESSION['user_role'] != 'admin') 
+if (!isset($_SESSION['user_id'])) 
     header("Location: login.php?redirect=admin.php");
+if (!$_SESSION['is_admin']) 
+    header('Location: 401.php');
 
 $database = new DatabaseService();
 $personalePageAdmin = Templating::getHtmlWithModifiedMenu(__FILE__);
 if (!$personalePageAdmin) {}
     //TODO
 
-$arrayMostreMuseo = $database->selectMostreCorrenti();
+$arrayMostreCorrenti = $database->selectMostreCorrenti();
+$arrayMostreFuture = $database->selectMostreFuture();
+$arrayMostrePassate = $database->selectMostrePassate();
+$arrayMostreMuseo = array_merge($arrayMostreFuture, $arrayMostreCorrenti, $arrayMostrePassate);
 $sectionMostreMuseoToAdmin = Templating::getContentBetweenPlaceholders($personalePageAdmin, "mostreAdmin");
 
 $fullcontent = "";
 foreach ($arrayMostreMuseo as $associativeRow) {
     $temp = $sectionMostreMuseoToAdmin;
-    Templating::replaceAnchor($temp, "nome_mostra", $associativeRow["nome"]);
-    Templating::replaceAnchor($temp, "descrizione_mostra", $associativeRow["descrizione"]);
+    Templating::replaceAnchor($temp, "nome", $associativeRow["nome"]);
+    Templating::replaceAnchor($temp, "descrizione", $associativeRow["descrizione"]);
     Templating::replaceAnchor($temp, "data_inizio", $associativeRow["data_inizio"]);
     Templating::replaceAnchor($temp, "data_fine", $associativeRow["data_fine"]);
     Templating::replaceAnchor(
