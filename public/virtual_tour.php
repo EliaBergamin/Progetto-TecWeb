fff70aca4337dd2fc88ade88ff38681643b76b0f
EliaBergamin <eliberg031009@gmail.com>
2025-01-01 16:09:39 +0100

    aggiunto stile per disabled button

--- public/virtual_tour.php
+++ public/virtual_tour.php
@@ -2,14 +2,8 @@
 require_once("phplibs/databaseService.php");
 require_once("phplibs/templatingService.php");
 
-
-
 if (!isset($_SESSION['user_id'])) 
     header("Location: login.php?redirect=virtual_tour.php");
-
-if ($_SESSION['is_admin'])
-    header("Location: 401.php");
-
 
 $database = new DatabaseService();
 $virtualTourHtmlContent = Templating::getHtmlWithModifiedMenu(__FILE__);
