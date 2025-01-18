043e6564fc1a84304884e57647f9d6d4c933f544
CodeUncas <peruzziuncas@gmail.com>
2025-01-18 14:29:01 +0100

    Fixato carosello e bottone scopri di più

--- public/index.php
+++ public/index.php
@@ -6,21 +6,49 @@ $indexHtmlContent = Templating::getHtmlWithModifiedMenu(__FILE__);
 
 $database = new DatabaseService();
 $arrayMostreCorrenti = $database->selectMostreCorrenti(false);
-if (count($arrayMostreCorrenti) != 3) // < 3
-    $arrayMostreFuture= $database->selectMostreFuture(false);
-unset($database);
+$arrayMostreFuture= $database->selectMostreFuture(false);
+$arrayMostrePassate = $database->selectMostrePassate(false);
 
+$defaultCarouselArray = array(
+    array(
+        "id_mostra" => -1,
+        "nome" => "Carousel Default 1",
+        "img_path" => "../images/car1.webp",
+        "alt" => ""
+    ),
+    array(
+        "id_mostra" => -2,
+        "nome" => "Carousel Default 2",
+        "img_path" => "../images/car2.webp",
+        "alt" => ""
+    ),
+    array(
+        "id_mostra" => -3,
+        "nome" => "Carousel Default 3",
+        "img_path" => "../images/car3.webp",
+        "alt" => ""
+    )
+);
+unset($database);
+$arrayMostreMerged = array_merge($arrayMostreCorrenti, $arrayMostreFuture, $arrayMostrePassate);
+$arrayMostreMerged = count($arrayMostreMerged) < 3 ? array_merge($arrayMostreMerged, $defaultCarouselArray) : $arrayMostreMerged;
 $carosello = Templating::getContentBetweenPlaceholders($indexHtmlContent, "carosello");
-for ($i = 0; $i < count($arrayMostreCorrenti); $i++) {
-    Templating::replaceAnchor($carosello, "img{$i}", $arrayMostreCorrenti[$i]["img_path"]);
-    Templating::replaceAnchor($carosello, "alt{$i}", $arrayMostreCorrenti[$i]["alt"]);
-    Templating::replaceAnchor($carosello, "id{$i}", $arrayMostreCorrenti[$i]["id_mostra"]);
+for ($i = 0; $i < 3; $i++) {
+    Templating::replaceAnchor($carosello, "img{$i}", $arrayMostreMerged[$i]["img_path"]);
+    Templating::replaceAnchor($carosello, "alt{$i}", $arrayMostreMerged[$i]["alt"]);
+    Templating::replaceAnchor($carosello, "id{$i}", $arrayMostreMerged[$i]["id_mostra"]);
 }
-for ($i = count($arrayMostreCorrenti); $i < 3; $i++) {
-    Templating::replaceAnchor($carosello, "img{$i}", $arrayMostreFuture[$i - count($arrayMostreCorrenti)]["img_path"]);
-    Templating::replaceAnchor($carosello, "alt{$i}", $arrayMostreFuture[$i - count($arrayMostreCorrenti)]["alt"]);
-    Templating::replaceAnchor($carosello, "id{$i}", $arrayMostreFuture[$i - count($arrayMostreCorrenti)]["id_mostra"]);
-}
+// for ($i = 0; $i < count($arrayMostreCorrenti); $i++) {
+//     Templating::replaceAnchor($carosello, "img{$i}", $arrayMostreCorrenti[$i]["img_path"]);
+//     Templating::replaceAnchor($carosello, "alt{$i}", $arrayMostreCorrenti[$i]["alt"]);
+//     Templating::replaceAnchor($carosello, "id{$i}", $arrayMostreCorrenti[$i]["id_mostra"]);
+// }
+// for ($i = count($arrayMostreCorrenti); $i < 3; $i++) {
+//     Templating::replaceAnchor($carosello, "img{$i}", $arrayMostreFuture[$i - count($arrayMostreCorrenti)]["img_path"]);
+//     Templating::replaceAnchor($carosello, "alt{$i}", $arrayMostreFuture[$i - count($arrayMostreCorrenti)]["alt"]);
+//     Templating::replaceAnchor($carosello, "id{$i}", $arrayMostreFuture[$i - count($arrayMostreCorrenti)]["id_mostra"]);
+// }
+
 Templating::replaceContentBetweenPlaceholders($indexHtmlContent, "carosello", $carosello);
 Templating::showHtmlPageWithoutPlaceholders($indexHtmlContent);
 ?>
